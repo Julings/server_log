fdead0e3e64a913aba871820adb3361ea6a659ef
Julings <tx_xiaowu@sina.com>
2019-06-15 11:46:10 +0800

    Code init.

---
+++ export_log.sh
@@ -0,0 +1,11 @@
+project_path = YOUR_PORJECT_PATH
+sa_log_path = YOUR_SA_LOG_PATH 
+#sa_log_path=/var/log/sa/
+
+cd ${project_path}
+
+sar -u -f ${sa_log_path}sa`date -d "1 day ago" +%d` > u_`date -d "1 day ago" +%Y%m%d`.log
+sar -r -f ${sa_log_path}sa`date -d "1 day ago" +%d` > r_`date -d "1 day ago" +%Y%m%d`.log
+sar -n DEV -f ${sa_log_path}sa`date -d "1 day ago" +%d` > n_`date -d "1 day ago" +%Y%m%d`.log
+sar -d -f ${sa_log_path}sa`date -d "1 day ago" +%d` > d_`date -d "1 day ago" +%Y%m%d`.log
+
